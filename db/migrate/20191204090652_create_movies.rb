e6a111105372a0add7d0a435317b10bb3a654d16
jack <jack@gmail.com>
2019-12-04 15:03:01 +0530

    Insert data on rails console to Database

--- db/migrate/20191204090652_create_movies.rb
+++ db/migrate/20191204090652_create_movies.rb
@@ -2,7 +2,7 @@ class CreateMovies < ActiveRecord::Migration[6.0]
   def change
     create_table :movies do |t|
       t.string :title
-      t.sting :rating
+      t.string :rating
       t.integer :total_gross
       t.text :description
       t.datetime :released_on
