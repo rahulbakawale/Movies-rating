734329ae153504dbae7313462012e1c22a841b4d
jack <jack@gmail.com>
2019-12-05 15:16:39 +0530

    not found Get 1/edit

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,14 @@
 module MoviesHelper
+
+  def time_date(date)
+    return  date.strftime('%d/%B/%Y : %H:%M %p') if date
+     "Coming Soon"
+  end
+
+   def number_change(num)
+    return number_to_currency(num)
+   end
+
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get '/movies' => 'movies#index'
+   get '/movies' => 'movies#index'
+   get '/movies/:id' => 'movies#show', as: 'movie'
+   get  "/movies/:id/edit" => 'movies#edit',as:'movie_edit'
+   root :to => 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
