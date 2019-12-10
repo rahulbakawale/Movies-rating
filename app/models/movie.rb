6cea3023f9a3e4451dda5278a9f0b0854f7dcc37
jack <jack@gmail.com>
2019-12-10 16:25:53 +0530

    movie controller in the update method

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,9 @@
 class Movie < ApplicationRecord
   def self.upcoming
-   where("released_on >= ?",Time.now).order('released_on')
+   where("released_on >= ? OR released_on IS NULL",Time.now).order('released_on')
   end
 
   def self.past
-    where("released_on < ?",Time.now).oder('released_on DESC')
+    where("released_on < ?",Time.now).order('released_on DESC')
    end 
 end
