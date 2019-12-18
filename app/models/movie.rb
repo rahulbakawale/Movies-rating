70e239072a64aa36bc0981efb5865792bf0e255c
jack <jack@gmail.com>
2019-12-18 14:34:40 +0530

    in model  Movie.rb  create () sold_out

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -11,7 +11,9 @@ class Movie < ApplicationRecord
     message: "must be a JPG or PNG image " }
 
    validates_numericality_of :total_gross, :only_integer => true, :greater_than_or_equal_to => 0
-  
+   
+
+
     #RATINGS = {1=>'G', 2=>'PG' ,3=>'PG-13',4=>'R',5=>'NC-17'}
    RATINGS = %w(G PG PG-13 R NC-17)
 
@@ -22,6 +24,15 @@ class Movie < ApplicationRecord
   def self.past
     where("released_on < ?",Time.now).order('released_on DESC')
    end 
+
+
+   
+  def sold_out?
+      
+    avaivility = spot - reviews.persisted_records.count
+    avaivility.zero? || avaivility < 0
+  end
+  
 
 end
 
