c9fca5a1174a31bf4d0d111e966eac8da93531c7
jack <jack@gmail.com>
2019-12-18 14:36:10 +0530

    some validation for custom througth

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,22 @@
 class Review < ApplicationRecord
   belongs_to :movie
+
+  #validate :check_spot_for_movie
+ 
+  scope :persisted_records, -> { where("id IS NOT NULL") }
+
+
+  def persisted_records
+    where "id IS NOT NULL"
+  end
+
+
+  private 
+
+ # def check_spot_for_movie
+ #   if movie.sold_out?
+ #       errors.add(:base, 'No More Review.')
+ #     end
+ # end
+
 end
