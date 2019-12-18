6b7dc38c3687eab73d58d4a32ef58fab23161c7d
jack <jack@gmail.com>
2019-12-18 14:32:57 +0530

    in Reviews helper one method create

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,9 @@
 module ReviewsHelper
+  def review_or_full(movie)
+    if movie.sold_out?
+      content_tag :span , "No More Reviews", class: 'sold_out'
+    else
+      link_to "Reviews ", new_movie_review_path(@movie), class: 'btn btn-primary'
+    end
+  end
 end
