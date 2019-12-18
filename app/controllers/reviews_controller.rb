0b28926a18013d1c1dce9e721812f63127a62057
jack <jack@gmail.com>
2019-12-18 14:31:20 +0530

    some private method in reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,42 @@
 class ReviewsController < ApplicationController
+  before_action :set_movie
+  before_action  :check_review_abl, only: [:new]
+  
   def index
-    
-    @movie=Movie.find(params[:movie_id])
     @reviews=@movie.reviews
   end
-end
+
+  def new 
+   @review=@movie.reviews.new
+  end
+
+  def create
+   @review=@movie.reviews.new(review_params)
+    if (@review.save)
+      flash[:notice]="Your Review successfully Insert "
+      redirect_to movie_reviews_path(@movie)
+     else
+      render :new 
+   end
+  end 
+
+  private
+
+  def review_params
+    params.require(:review).permit(:name,:stars,:comment)
+   end
+   
+   def set_movie
+    @movie=Movie.find(params[:movie_id]) 
+  end
+
+   def check_review_abl
+    if @movie.sold_out?
+      flash[:errors] ="No more review "
+      redirect_to movie_path(@movie) and return
+     end 
+   end
+end  
 
 
 
