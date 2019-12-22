7989e5d65443ab50eb0d1a40e3ad464486f55324
jack <jack@gmail.com>
2019-12-22 23:00:59 +0530

    router resoures :users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  
+  resources :users
    #get '/movies' => 'movies#index'
    #get '/movies/new'  => 'movies#new', as: 'new_movie'
    #get '/movies/:id' => 'movies#show', as: 'movie'
